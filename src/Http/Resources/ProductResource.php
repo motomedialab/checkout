e6feab55f845598b5604f5079f32ba646910be50
Chris Page <chris@motocom.co.uk>
2022-07-26 16:32:10 +0100

    Added discount in pence to product resources when a voucher has been applied

--- src/Http/Resources/ProductResource.php
+++ src/Http/Resources/ProductResource.php
@@ -2,10 +2,13 @@
 
 namespace Motomedialab\Checkout\Http\Resources;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Motomedialab\Checkout\Contracts\AppliesVoucher;
 use Motomedialab\Checkout\Enums\ProductStatus;
 use Motomedialab\Checkout\Helpers\VatCalculator;
 use Motomedialab\Checkout\Models\Product;
+use Motomedialab\Checkout\Models\Voucher;
 
 /**
  * @property-read Product $resource
@@ -15,6 +18,12 @@ class ProductResource extends JsonResource
     public function toArray($request)
     {
         $currency = strtoupper($request->get('currency', config('checkout.default_currency')));
+        
+        try {
+            $voucher = $request->has('voucher') ? Voucher::findByCode($request->get('voucher')) : null;
+        } catch (ModelNotFoundException $e) {
+            $voucher = new Voucher(['value' => 0]);
+        }
         
         return [
             'id' => $this->resource->getKey(),
@@ -33,6 +42,10 @@ class ProductResource extends JsonResource
                 $this->resource->relationLoaded('basket')
                     ? $this->resource->basket->vat_rate
                     : $this->resource->vat_rate
+            ),
+            'discount_in_pence' => $this->when(
+                $voucher instanceof Voucher,
+                fn() => app(AppliesVoucher::class)(collect([$this->resource]), $voucher, $currency)
             ),
             'available' => $this->resource->status === ProductStatus::AVAILABLE,
             'children' => static::collection($this->whenLoaded('children')),
